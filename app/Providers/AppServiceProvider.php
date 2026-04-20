b3f7ba8793a311acffb6f12b1bc2d5543cd72a15
antlogist <antlogist@yandex.ru>
2026-04-20 14:58:36 +0300

    feat: add download reports gate #29

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use App\Services\QueuedZipUploader;
 use App\Services\ZipUploader;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,5 +28,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
+
+        Gate::define('download reports', function (User $user) {
+            return $user->role === 'admin';
+        });
     }
 }
